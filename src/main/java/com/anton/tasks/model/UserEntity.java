22862bc692039a5cb2bc1f42fb06af749ecbd4f5
Anton <amikhalchenkonton@gmail.com>
2026-09-21 15:34:19 +0200

    feat: prepare user password storage

--- src/main/java/com/anton/tasks/model/UserEntity.java
+++ src/main/java/com/anton/tasks/model/UserEntity.java
@@ -10,12 +10,19 @@ public class UserEntity {
     private Long id;
     @Column(nullable = false, unique = true, length = 50)
     private String username;
+    @Column(name = "password_hash")
+    private String passwordHash;
 
     protected UserEntity() {
     }
 
     public UserEntity(String userName) {
         this.username = userName;
+    }
+
+    public UserEntity(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
     }
 
     public Long getId() {
@@ -26,4 +33,7 @@ public class UserEntity {
         return username;
     }
 
+    public String getPasswordHash() {
+        return passwordHash;
+    }
 }
